c03759d1499de05f841df0c82eb4247a14d5fc03
Kevin Garzón <santiagomazabel04@outlook.com>
2025-10-17 09:51:22 -0500

    Puse el controlador de interacciones global para las interacciones de todos los modulos

--- app/Http/Controllers/InteraccionController.php
+++ app/Http/Controllers/InteraccionController.php
@@ -3,17 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Models\OfertaLaboral;
+use App\Models\Formacion;
 use Illuminate\Http\Request;
 
 class InteraccionController extends Controller
 {
-    public function registrar($id)
+    /**
+     * Registra una interacción en una oferta laboral
+     */
+    public function registrarOferta($id)
     {
         $oferta = OfertaLaboral::find($id);
 
         if ($oferta) {
             $oferta->increment('interacciones');
             return response()->json(['success' => true, 'interacciones' => $oferta->interacciones]);
+        }
+
+        return response()->json(['success' => false], 404);
+    }
+
+    /**
+     * Registra una interacción en un programa de formación
+     */
+    public function registrarFormacion($id)
+    {
+        $formacion = Formacion::find($id);
+
+        if ($formacion) {
+            $formacion->increment('interacciones');
+            return response()->json(['success' => true, 'interacciones' => $formacion->interacciones]);
         }
 
         return response()->json(['success' => false], 404);
