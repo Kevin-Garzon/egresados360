7991c4b7a3856402be0b6b1377d4c8a475b3e655
Kevin Garzón <santiagomazabel04@outlook.com>
2025-11-10 23:19:43 -0500

    Agregue el meta viewport para que tailwind reconozca adecuamente el break y pase a la version movil en las vistas admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Panel Admin — Egresados 360')</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
     <link rel="icon" type="image/png" href="{{ asset('fet.png') }}">
